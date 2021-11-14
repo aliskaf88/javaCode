4858e72111dcde89884b5a5e642e41e3dafbaaac
aliskaf88 <alyskaf@gmail.com>
2021-11-14 12:39:11 +0100

    Update Horloge1.java

--- Horloge1.java
+++ Horloge1.java
@@ -6,7 +6,7 @@
  ******************************************************************************/
 import java.util.* ;
 
-public class Horloge {
+public class Horloge1 {
         
     public static void main(String[] args) { 
 	    StdDraw.enableDoubleBuffering();
